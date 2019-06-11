9613402e53f51c5fc496314850cc52dd091e9c95
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2019-06-12 01:22:56 +0200

    Add exception handling and use unique variable names to prevent name clash

--- python-repl.py
+++ python-repl.py
@@ -1,13 +1,23 @@
-import sys
-import code
+import sys as kakoune_python_bridge_sys
+import code as kakoune_python_bridge_code
+import traceback as kakoune_python_bridge_traceback
 
-input = sys.argv[1]
-output = sys.argv[2]
+kakoune_python_bridge_input = kakoune_python_bridge_sys.argv[1]
+kakoune_python_bridge_output = kakoune_python_bridge_sys.argv[2]
 
-i = code.InteractiveInterpreter()
+kakoune_python_bridge_i = kakoune_python_bridge_code.InteractiveInterpreter()
 while True:
-    with open(input, 'r') as f:
-        c = f.read()
-        sys.stdout = open(output, 'w')
-        i.runsource(c)
-        sys.stdout.close()
+    with open(kakoune_python_bridge_input, 'r') as kakoune_python_bridge_f:
+        kakoune_python_bridge_c = kakoune_python_bridge_f.read()
+        try:
+            kakoune_python_bridge_sys.stdout = open(kakoune_python_bridge_output, 'w')
+            if not kakoune_python_bridge_i.runsource(kakoune_python_bridge_c):
+                try:
+                    exec(kakoune_python_bridge_c)
+                except Exception:
+                    kakoune_python_bridge_sys.stderr = kakoune_python_bridge_sys.stdout
+                    kakoune_python_bridge_traceback.print_exc()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            kakoune_python_bridge_sys.stdout.close()
